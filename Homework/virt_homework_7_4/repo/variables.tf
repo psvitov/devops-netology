103d43bdd6c61c1c48d6c59441a8d08298588056
Pavel Svitov <95366264+psvitov@users.noreply.github.com>
2022-07-14 19:50:35 +0400

    Add files via upload

---
+++ Homework/virt_homework_7_4/repo/variables.tf
@@ -0,0 +1,27 @@
+variable "yc_token" {
+   description = "OAuth-token Yandex.Cloud"
+   default = "AQAAAAAARMfEAATuwQ3i5zgf2kUokcys-7P6_1k"
+}
+
+variable "yc_cloud_id" {
+   description = "ID Yandex.Cloud"
+   default = "b1g8fhcp9qmijeuurdt7"
+}
+
+variable "yc_folder_id" {
+   description = "ID folder terraform"
+   default = "b1g9ofom2ntbfc8shnlh"
+}
+
+variable "yc_region" {
+   description = "Region Zone"
+   default = "ru-central1-a"
+}
+#variable "yc_access_key" {
+#   description = "Bucket access key"
+#   default = "YCAJElDQ8xPTm97SIzOSc09d7"
+#}
+#variable "yc_secret_key" {
+#   description = "Bucket secret key"
+#   default = "YCOP0AgYp0knigY7qvIf4JZA2I8GN7lOOC-oaky5"
+#}
