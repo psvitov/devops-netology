b2a627ae338e703f82d7dfe6fe2b39a92a6e6f71
Pavel Svitov <95366264+psvitov@users.noreply.github.com>
2023-04-05 19:58:46 +0400

    Create main.tf

---
+++ Diplom/jenkins/main.tf
@@ -0,0 +1,17 @@
+## main.tf
+
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+      version = "0.61.0"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  token     = var.yc_token
+  cloud_id  = var.yc_cloud_id
+  zone      = var.yc_region_a
+}
