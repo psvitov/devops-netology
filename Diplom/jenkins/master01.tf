22933e88ec019ff5cc279e9d191e4c53846025fe
Pavel Svitov <95366264+psvitov@users.noreply.github.com>
2023-04-05 19:59:27 +0400

    Create master01.tf

---
+++ Diplom/jenkins/master01.tf
@@ -0,0 +1,29 @@
+## master01.tf
+
+resource "yandex_compute_instance" "jenkins-master01" {
+  folder_id = "b1g51av8v48qip32jg1s"
+  name = "jenkins-master-01"
+  zone = var.yc_region_a
+  hostname = "jenkins-master-01.ru-central1.internal"
+
+  resources {
+    cores  = 2
+    memory = 4
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8jvcoeij6u9se84dt5"
+      size = "20"
+    }
+  }
+
+  network_interface {
+    subnet_id = "e9bjdahvi925mpo4sj4v"
+    nat = true
+  }
+
+  metadata = {
+    user-data = "${file("./meta.txt")}"
+  }
+}
