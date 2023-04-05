8bc06248c93de47178692cb468510efb97c062b2
Pavel Svitov <95366264+psvitov@users.noreply.github.com>
2023-04-05 19:57:17 +0400

    Create inventory.tf

---
+++ Diplom/jenkins/inventory.tf
@@ -0,0 +1,31 @@
+## inventory.tf
+
+resource "local_file" "inventory" {
+  content = <<-DOC
+  ---
+  all:
+    hosts:
+      jenkins-master-01:
+        ansible_host: ${yandex_compute_instance.jenkins-master01.network_interface.0.nat_ip_address}
+      jenkins-agent-01:
+        ansible_host: ${yandex_compute_instance.jenkins-agent01.network_interface.0.nat_ip_address}
+    children:
+      jenkins:
+	children:
+          jenkins_masters:
+            hosts:
+              jenkins-master-01:
+          jenkins_agents:
+            hosts:
+              jenkins-agent-01:
+    vars:
+      ansible_connection_type: paramiko
+      ansible_user: au000846
+     DOC
+  filename = "./infrastructure/inventory/cicd/hosts.yml"
+
+  depends_on = [
+    yandex_compute_instance.jenkins-master01,
+    yandex_compute_instance.jenkins-agent01
+  ]
+}
