80ae79c571ff3f4a425ac36ddd439562cbcfb156
Pavel Svitov <95366264+psvitov@users.noreply.github.com>
2023-04-18 22:17:24 +0400

    Update variables.tf

--- Diplom/final/terraform/variables.tf
+++ Diplom/final/terraform/variables.tf
@@ -14,5 +14,35 @@ variable "yc_cloud_id" {
 
 variable "yc_region_a" {
     description = "Region Zone A"
-    default = "<Зона доступности>"
+    default = "ru-central1-a"
+}
+
+variable "yc_region_b" {
+    description = "Region Zone B"
+    default = "ru-central1-b"
+}
+
+variable "yc_region_c" {
+    description = "Region Zone C"
+    default = "ru-central1-c"
+}
+
+variable "master" {
+    description = "Initial Master-node name"
+    default = "<имя master-ноды>"
+}
+
+variable "masters_count" {
+    description = "Quantity of master-node"
+    default = "<количество master-нод>"
+}
+
+variable "node" {
+    description = "Initial Worker-node name"
+    default = "<имя worker-ноды>"
+}
+
+variable "nodes_count" {
+    description = "Quantity of worker-node"
+    default = "<количество worker-нод>"
 }
