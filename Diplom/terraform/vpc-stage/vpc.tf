17d45578593772ba42eb683ff6b79ede2f2930ab
Pavel Svitov <95366264+psvitov@users.noreply.github.com>
2023-04-18 13:17:36 +0400

    Update vpc.tf

--- Diplom/terraform/vpc-stage/vpc.tf
+++ Diplom/terraform/vpc-stage/vpc.tf
@@ -1,45 +1,30 @@
-resource "yandex_compute_instance_group" "stage-ig" {
-  name               = "vpc-stage-ig"
-  folder_id          = "${yandex_resourcemanager_folder.folder1.id}"
-  service_account_id = "${yandex_iam_service_account.sa.id}"
-  depends_on          = [yandex_resourcemanager_folder_iam_member.editor]
-  instance_template {
-    platform_id = "standard-v1"
-    resources {
-      memory = 2
-      cores  = 2
-    }
+## vpc-stage
 
-    boot_disk {
-      mode = "READ_WRITE"
-      initialize_params {
-        image_id = "fd8jvcoeij6u9se84dt5"
-      }
-    }
+resource "yandex_compute_instance" "vpc" {
+  count = var.vpc_count
+  folder_id = "${yandex_resourcemanager_folder.folder1.id}"
+  name = "${var.vpc}-${count.index+1}"
+  zone = var.yc_region_a
+  hostname = "${var.vpc}-${count.index+1}.ru-central1.internal"
 
-    network_interface {
-      network_id = "${yandex_vpc_network.stage-net.id}"
-      subnet_ids = ["${yandex_vpc_subnet.stage-subnet-a.id}", "${yandex_vpc_subnet.stage-subnet-b.id}", "${yandex_vpc_subnet.stage-subnet-c.id}"]
-      nat = "true"
-    }
+  resources {
+    cores  = 2
+    memory = 2
+  }
 
-    metadata = {
-      ssh-keys = "devops:ssh-rsa AAAAB3NzaC1yc2E*************************/ZITHZlVbTVaFNOPIOVRgNc7mRRQ4+3CbJIwYTumH0pJal7Rc6CeTXFEb35HeMoO0eyEzan4TSOfXvS4Q5Wutq8CRAbbdG7UwrUGcMeK0U6XAQguuxVcY5C7bGJ8c2y2In19fM0TpheLA1c1LZeX2BXJbIXTJM52Fsfo0s0t6pXPDLXx/2PkdeFdrMhUybJ5202SQXfbsElgDsVCfaEkWMm8gJfsIzMqLWEHrheFtfcbLIRzpBvEs2OZbK6Fofo7akYy5zkjvOrGaVy4u9YoFStkD2uCGOp8D8q3ADULYEunI+INVTdg/JtBRjYBw60ZH devops@DevOps"
+  boot_disk {
+    initialize_params {
+      image_id = "fd8jvcoeij6u9se84dt5"
+      size = "10"
     }
   }
 
-  scale_policy {
-    fixed_scale {
-      size = 3
-    }
+  network_interface {
+    subnet_id = "${yandex_vpc_subnet.stage-subnet-a.id}"
+    nat = true
   }
 
-  allocation_policy {
-    zones = [var.yc_region_a, var.yc_region_b, var.yc_region_c]
-  }
-
-  deploy_policy {
-    max_unavailable = 1
-    max_expansion = 0
+  metadata = {
+    user-data = "${file("./meta.txt")}"
   }
 }
